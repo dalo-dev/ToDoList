ac3c74b129a741a13ca331e191ec412aad29607b
DALO <dalodev@proton.me>
2024-03-26 15:16:01 -0500

    Added LocalStorage DB

--- script.js
+++ script.js
@@ -8,19 +8,34 @@ const listElement = document.querySelector(".list");
 
 //////////////////
 
-const displayTask = function (task) {
+let taskList = JSON.parse(localStorage.getItem("tasks"));
+
+const displayTask = function (task, order) {
   const htmlElement = `<li>
     <p>${task}</p>
     </li>`;
-  listElement.insertAdjacentHTML("afterBegin", htmlElement);
+  listElement.insertAdjacentHTML(order, htmlElement);
   inputElement.value = "";
+  updateLocalStorage();
 };
+
+const updateLocalStorage = function () {
+  const tasksElements = listElement.querySelectorAll("li");
+  const tasks = [];
+
+  tasksElements.forEach((task) => {
+    tasks.push({ task: task.innerText });
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+//////////////////
+
+taskList &&
+  taskList.forEach((taskItem) => displayTask(taskItem.task, "beforeend"));
 
 formElement.addEventListener("submit", (e) => {
   e.preventDefault();
   const task = inputElement.value.trim();
-
-  if (task) {
-    displayTask(task);
-  }
+  task && displayTask(task, "afterBegin");
 });
